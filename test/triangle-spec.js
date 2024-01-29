428305f48d2153c9e6f4c1dc78c3f1303cfd5d71
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2024-01-28 22:56:29 -0500

    For the Triangle class, we make tests for the constructor.

--- test/triangle-spec.js
+++ test/triangle-spec.js
@@ -7,14 +7,36 @@ const spies = require('chai-spies');
 
 describe('Triangle', function () {
     // Please use hooks this time
+    let triangle;
+
     describe('Constructor', function () {
 
-        it('should accept 3 arguments: side1, side2, side3', function () {
+        this.beforeEach(function () {
+            [side1, side2, side3] = [3, 3, 3];
+            triangle = new Triangle(side1, side2, side3);
+        });
 
-        })
-    })
+        it('should create an instance of the triangle class', function () {
+            expect(triangle).to.exist;
+            expect(triangle).to.be.instanceOf(Triangle);
+        });
 
-})
+        it('should initialize 3 arguments: side1, side2, side3', function () {
+            expect(triangle.side1).to.exist;
+            expect(triangle.side2).to.exist;
+            expect(triangle.side3).to.exist;
+        });
+
+    });
+
+    describe('getPerimeter' , function () {
+
+        it('should return the sum of all three sides', function () {
+
+        });
+    });
+
+});
 
 describe('Scalene', function () {
 
