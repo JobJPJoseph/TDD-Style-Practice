f77f257c07121df69d3320cf2a5efa3f93ece770
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2024-02-02 22:11:29 -0500

    We our first test using spy to check for existence of arguments

--- test/my-map-spec.js
+++ test/my-map-spec.js
@@ -1,1 +1,54 @@
 // Your code here
+const { expect } = require('chai');
+const { myMap } = require('../problems/my-map');
+const spies = require('chai-spies');
+const chai = require('chai');
+
+chai.use(spies);
+
+describe('myMap', function () {
+
+    let arrayTypeNums;
+    let emptyArray;
+    let arrayTypeStr;
+    let stringType;
+
+    let justReturn;
+    let multiply;
+    let addYay;
+
+    this.beforeEach(function () {
+        arrayTypeNums = [1, 2, 3, 4];
+        arrayTypeStr = ['a', 'b', 'c', 'd'];
+        emptyArray = [];
+        stringType = "I am a string";
+
+        justReturn = (input) => {
+            return input;
+        }
+
+        multiply = (input) => {
+            return input * 2;
+        }
+
+        addYay = (input) => {
+            return input + "yay";
+        }
+
+    });
+
+    it('should take two arguments: input array and a callback', function () {
+        // Use chai.spy.on to spy on the function
+        const myMapSpy = chai.spy.on(myMap);
+
+        myMapSpy(arrayTypeNums, justReturn);
+
+        // Use chai.spy.on to make assertions about the spy
+        expect(myMapSpy).to.have.been.called.with(arrayTypeNums, justReturn);
+    });
+
+    it('should throw an error if first argument is not an array type', function () {
+        expect(() => myMap(stringType, justReturn)).to.throw(Error);
+    });
+
+});
